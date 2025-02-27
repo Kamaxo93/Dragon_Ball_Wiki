7b8d37fc0234f45cef3a29078a17339659f6001b
kamaxo <kamaxo93@gmail.com>
2025-02-27 14:22:36 +0100

    implementation testing

--- app/src/test/java/com/example/dragonballwiki/dragonlist/data/repository/DragonListRepositoryTest.kt
+++ app/src/test/java/com/example/dragonballwiki/dragonlist/data/repository/DragonListRepositoryTest.kt
@@ -8,12 +8,15 @@ import com.example.dragonballwiki.dragonlist.domain.model.CharacterBO
 import com.example.dragonballwiki.dragonlist.domain.repository.DragonListRepository
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.coJustRun
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.justRun
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -30,14 +33,34 @@ class DragonListRepositoryTest {
 
     @Before
     fun onBefore() {
-        MockKAnnotations.init(this)
         dragonListRepository = DragonListRepositoryImpl(remoteDataSource = dragonListRemoteDataSource, localDataSource = dragonListLocalDataSource, sharedPreferences = sharedPreferences)
     }
 
     @Test
-    fun `when the api returns success and returns a list with dragon ball list`(): Unit = runBlocking {
+    fun `when the api returns success and returns a list with dragon ball list`() = runTest {
         //Given
         every { dragonListLocalDataSource.getCharactersList() } returns flowOf(emptyList())
+        coEvery { dragonListRemoteDataSource.getCharacterList() } returns listOf(
+            CharacterBO(
+                affiliation = "imperdiet",
+                description = "enim",
+                gender = "purus",
+                id = 5864,
+                image = "referrentur",
+                ki = "honestatis",
+                maxKi = "wisi",
+                name = "Rodrigo Odom",
+                race = "molestiae"
+
+            )
+        )
+        every { sharedPreferences.getLong(any(), any()) } returns 0
+        justRun {
+            sharedPreferences.edit().putLong(any(), any()).apply()
+        }
+        coJustRun {
+            dragonListLocalDataSource.addCharacters(any())
+        }
 
         //When
         dragonListRepository.getCharacterList()
